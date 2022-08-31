2466673450b0a942fb3b4cf6060c30f81ad74aef
Nguyen Quan <quan.nguyendinh@ncc.asia>
2022-08-31 17:17:44 +0700

    fix ui and bug

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 import * as hbs from 'hbs';
 import * as dayjs from 'dayjs';
 import * as cookieParser from 'cookie-parser';
+import { Reaction } from './Reaction/reaction.schema';
 var moment = require('moment');
 const a = moment().format('MM DD YYYY, HH:mm:SS a');
 
@@ -34,10 +35,28 @@ const formatDay = (time) => {
     return dayjs(+new Date(+time.toString())).format('DD/MM/YYYY hh:mm A');
   }
 };
+const maxReaction = (emotion : Reaction[], opts) : Reaction[]=>{
+  return emotion.length > 3 ? opts.fn(emotion.sort((a, b)=>b.count - a.count).filter((value, index)=>index < 3)) : opts.fn(emotion);
+}
+
+const feel = (post)=>{
+  return post.totalLike + post.reactions.reduce((sumReaction, reaction)=>{
+    return sumReaction + reaction.count;
+  }, 0);
+}
+const maxPosts = (posts, opts) =>{
+  //get posts if time posts < 1 weeks
+   const maxPosts = posts.filter((posts)=> new Date((new Date().getTime()- new Date(+posts.createdTimestamp).getTime())).getDate()  <= 31)
+  return maxPosts.length > 3 ? opts.fn(maxPosts.sort((a, b)=>feel(b)- feel(a)).filter((value, index)=>index < 9 )) : opts.fn(maxPosts);
+}
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   hbs.registerHelper('formatDay', formatDay);
+  hbs.registerHelper('maxReaction', maxReaction);
+  hbs.registerHelper('maxPosts', maxPosts);
+  hbs.registerHelper('feel', feel);
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
