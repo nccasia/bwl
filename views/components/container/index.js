00636414754f11a24ea2a8221c88b7e4f604d041
dat2 <dat.lethanh2@ncc.asia>
2023-06-14 13:11:13 +0700

    fix noticatin imageimage

--- views/components/container/index.js
+++ views/components/container/index.js
@@ -6,7 +6,6 @@ import React from 'react';
 
 const Container = () => {
   const { state, dispatch } = useStore();
-  console.log("hello: ", state.posts);
   return (
     <div className="container-list">
       {state.posts?.map((post, index) => (
